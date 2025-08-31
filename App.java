251e5c08fb6ddecab5fd886c022ed79f931e013b
RedInfinity_Pro <danieltower101501@gmail.com>
2025-08-30 21:28:09 -0400

    2

--- App.java
+++ App.java
@@ -16,4 +16,4 @@ public class App {
 // 1. What advantage does inheritance give us compared to rewriting the same code in multiple classes?
     // This process allows the transfer of information across multiple files to be easier and faster; in addition, it demonstrates the importance of categorizing objects and what they inherently are.
 // 2. Could you imagine other subclasses of Person that would make sense (e.g., Teacher)? What attributes or methods might they have?
-    // Other subclasses that may be beneficial for determining the person may include employment status, career status of the individual, or educational background/history.
+    // Other subclasses that may be beneficial for determining the person may include employment status, career status of the individual, or educational background/history. For example, the person's career status might return values such as employment status, employment type, yearly wage, job description, and quite possibly skills obtained in the job.
